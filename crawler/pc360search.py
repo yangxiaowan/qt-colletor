62143213a49fe63b100909fac98d488ee73637c6
yangwan <yangwan@yingkelecai.com>
2018-01-04 10:10:08 +0800

    移动端爬虫

--- crawler/pc360search.py
+++ crawler/pc360search.py
@@ -9,7 +9,6 @@ import re
 搜狗搜索引擎爬虫
 '''
 
-
 class Pc360Search(MySearch):
     # 搜索排名
     page_index = 0
@@ -56,8 +55,10 @@ class Pc360Search(MySearch):
             if self.recommend_search_parseflag is False:
                 if other_search_div is not None:
                     self.parse_other_search(other_search_div)
+                    self.recommend_search_parseflag = True
             if self.relate_search_parseflag is False:
                 self.parse_relate_search(relate_search_div)
+                self.relate_search_parseflag = True
             self.parse_result_page(content_div)
 
     def parse_other_search(self, result):
@@ -90,11 +91,12 @@ class Pc360Search(MySearch):
 
     def parse_result_page(self, result):
         print("正在解析360页面......................")
-        res_list = result.find("li", attrs={'class': 'res-list'})
+        res_list = result.find_all("li", attrs={'class': 'res-list'})
         print("找到解析网站词条数目: ", len(res_list))
         for res_list_item in res_list:
             res_list_h3 = res_list_item.find("h3")
             if res_list_h3 is not None:
+                self.page_index += 1
                 craw_item = CrawlerItem()
                 setattr(craw_item, 'search', "PC端360")
                 setattr(craw_item, 'keyword', self.keyword)
@@ -103,10 +105,27 @@ class Pc360Search(MySearch):
                 res_list_h3_a = res_list_h3.find("a")
                 if res_list_h3_a is not None:
                     setattr(craw_item, 'title', res_list_h3_a.get_text().replace("\n", ""))
-                    setattr(craw_item, 'page', res_list_h3_a.get("href"))
+                    setattr(craw_item, 'page_url', res_list_h3_a.get("href"))
+                desc_content_p = res_list_item.find(attrs={'class': re.compile(".*(res-desc).*")})
+                if desc_content_p is not None:
+                    page_content = desc_content_p.get_text()
+                else:
+                    page_content_div = res_list_item.find("div")
+                    page_content = page_content_div.get_text()
+                setattr(craw_item, 'content', page_content.replace("\n", ""))
+                res_linkinfo_p = res_list_item.find(attrs={"class": "res-linkinfo"})
+                linkinfo = ''
+                if res_linkinfo_p is not None:
+                    linkinfo = res_linkinfo_p.find("cite").get_text()
+                else:
+                    temp_url = res_list_item.find(attrs={"class": "mh-showurl"}).find("cite")
+                    if temp_url is not None:
+                        linkinfo = temp_url.get_text()
+                setattr(craw_item, 'domain', linkinfo)
+                setattr(craw_item, 'index', self.page_index)
                 print(craw_item)
         print("解析360页面结束......................")
 
 
-test = Pc360Search("360彩票", 1)
+test = Pc360Search("360彩票", 10)
 test.genrate_pageurl()
