8f9802958ed1738aea9e7e6825c791eededb9ab7
yangwan <yangwan@yingkelecai.com>
2018-01-05 20:00:40 +0800

    添加生产者消费者多线程爬取策略

--- thread/writeexcelthread.py
+++ thread/writeexcelthread.py
@@ -5,5 +5,13 @@ from threading import Thread
 
 
 class WriteExcelThread(Thread):
-    def __init__(self):
+    '''
+        解析Excel线程!!!
+    '''
+
+    def __init__(self, con, product_queue):
+        self.condition = con
+        self.product_queue = product_queue
+
+    def run(self):
         pass
